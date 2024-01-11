c995af41273f6222c94972bc5b9d685012e6fe54
hscho <hscho@orange1.kr>
2024-01-11 13:20:21 +0900

    fix

--- src/PhoneFormatter.php
+++ src/PhoneFormatter.php
@@ -19,25 +19,23 @@ class PhoneFormatter
         private TypesInterface $Biz = new Biz,
         private TypesInterface $Disposable = new Disposable,
         private TypesInterface $Internet = new Internet,
-    )
-    {
+    ) {
     }
 
     public function change($phoneNumber): mixed
     {
         $phoneNumberPure = preg_replace('/\D/', '', $phoneNumber);
-        if($newNumber = $this->Local->change($phoneNumberPure)) {
+        if ($newNumber = $this->Local->change($phoneNumberPure)) {
             return $newNumber;
-        } elseif($newNumber = $this->Phone->change($phoneNumberPure)) {
+        } elseif ($newNumber = $this->Phone->change($phoneNumberPure)) {
             return $newNumber;
-        } elseif($newNumber = $this->Biz->change($phoneNumberPure)) {
+        } elseif ($newNumber = $this->Biz->change($phoneNumberPure)) {
             return $newNumber;
-        } elseif($newNumber = $this->Disposable->change($phoneNumberPure)) {
+        } elseif ($newNumber = $this->Disposable->change($phoneNumberPure)) {
             return $newNumber;
-        } elseif($newNumber = $this->Internet->change($phoneNumberPure)) {
+        } elseif ($newNumber = $this->Internet->change($phoneNumberPure)) {
             return $newNumber;
         }
         return $phoneNumber;
     }
-
 }
